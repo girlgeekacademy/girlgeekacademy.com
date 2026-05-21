22de0bdbb99c511c499f9c433c4826e6493eb4d2
Sarah Moran <sarah@patientnotes.app>
2026-05-21 22:16:57 +1000

    Imported initial home page from live site

---
+++ src/data/urls.ts
@@ -0,0 +1,18 @@
+export const SITE = 'https://girlgeekacademy.com';
+
+export const urls = {
+	home: `${SITE}/`,
+	about: `${SITE}/about/`,
+	aiHigh: `${SITE}/ai-high/`,
+	sheHacksSwift: `${SITE}/shehacksswift/`,
+	report: `${SITE}/submission-to-the-national-diversity-in-stem-review/`,
+	shop: `${SITE}/shop`,
+	welcomePack:
+		'https://drive.google.com/file/d/1K2kcxV6-anX0ZM0RHhSLXOcZ3wtHc-JW/view?usp=sharing',
+	twitter: 'https://twitter.com/girlgeekacademy',
+	facebook: 'https://www.facebook.com/girlgeekacademy/',
+	linkedin: 'https://www.linkedin.com/company/girlgeekacademy/',
+	youtube: 'https://www.youtube.com/girlgeekacademy',
+	instagram: 'https://instagram.com/girlgeekacademy',
+	email: 'mailto:hello@girlgeekacademy.com',
+} as const;
